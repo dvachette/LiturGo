f3370436b6f56c9d5f3dad44700c8dd0ac4e0b07
dvachette <donatien.vachette@hotmail.com>
2024-07-29 11:08:20 +0200

    Many changes in the project : logs, decorators...
    New templates and css files added
    Decorators were misplaced, and now works as expected
    New log file added

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # Created by : Donatien VACHETTE
-
-# Date : 10/06/2021
 
 # Description : This file is the main file of the project. It contains the main code of the project.
 
 # Importing the necessary libraries
 
 # Flask is used to manage web pages
-from flask import Flask, request, session, redirect, url_for, render_template, flash, abort
+import logging.config
+from flask import (
+    Flask,
+    request,
+    session,
+    redirect,
+    url_for,
+    render_template,
+    flash,
+    abort,
+)
 from flask_sqlalchemy import SQLAlchemy
+
+# Werkzeug is used to manage passwords and security
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Dataclass is used to create classes with attributes that can be used as a dictionary
@@ -38,7 +48,7 @@ DEBUG = True
 app = Flask(__name__)
 
 # Setting the secret key and the database URI
-app.secret_key = "MyOwnSuperSecretKeyThatNoOneElseShouldKnow"
+app.secret_key = 'MyOwnSuperSecretKeyThatNoOneElseShouldKnow'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -47,14 +57,57 @@ db = SQLAlchemy(app)
 
 # Logging configuration
 logging.basicConfig(level=logging.DEBUG)
-
+file_loging = logging.FileHandler('app.log')
+file_loging.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+file_loging.setFormatter(formatter)
+app.logger.addHandler(file_loging)
 
 # ==== Classes ==== #
 
+# ---- Dataclasses ---- #
 # Page list class
 @dataclass
-class pages:
-    pass
+class Templates:
+    """
+    Templates class
+
+    Template().page: str : the html code of the page
+    """
+
+    index: str = 'index.html'
+
+
+@dataclass
+class AdminRoutes:
+    """
+    AdminRoutes class
+
+    AdminRoutes().home: str : the route of the admin home page
+    """
+
+    home: str = 'admin_home'
+    login: str = 'login'
+    index: str = 'index'
+
+
+@dataclass
+class Routes:
+    """
+    Routes class
+
+    Routes().index: str : the route of the index page
+    Routes().login: str : the route of the login page
+    """
+
+    home: str = 'home'
+    login: str = 'login'
+    index: str = 'index'
+
+
+# ---- Database classes ---- #
 
 # "Paroisse" class
 class Paroisse(db.Model):
@@ -65,11 +118,13 @@ class Paroisse(db.Model):
     id : int : the id of the paroisse
     name : str : the name of the paroisse
     """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    
+
     def __repr__(self):
-        return f"<Paroisse {self.name}>"
+        return f'<Paroisse {self.name}>'
+
 
 # "Intervenant" class
 class Intervenant(db.Model):
@@ -94,11 +149,13 @@ class Intervenant(db.Model):
     phone = db.Column(db.String(100), nullable=False)
     desc = db.Column(db.String(100), nullable=False)
 
-    def is_role(self, group : str|list[str]) -> bool:
+    def is_role(self, group: str | list[str]) -> bool:
         if isinstance(group, str):
             _group = Role.query.filter_by(name=group).first()
             group_id = _group.id
-            roles = Intervenant_Role.query.filter_by(intervenant_id=self.id).all()
+            roles = Intervenant_Role.query.filter_by(
+                intervenant_id=self.id
+            ).all()
             for role in roles:
                 if role.role_id == group_id:
                     return True
@@ -108,15 +165,16 @@ class Intervenant(db.Model):
             for g in group:
                 _group = Role.query.filter_by(name=g).first()
                 group_id.append(_group.id)
-            roles = Intervenant_Role.query.filter_by(intervenant_id=self.id).all()
+            roles = Intervenant_Role.query.filter_by(
+                intervenant_id=self.id
+            ).all()
             for role in roles:
                 if role.role_id in group_id:
                     return True
             return False
-        
-    
+
     def __repr__(self):
-        return f"<Intervenant {self.name}>"
+        return f'<Intervenant {self.name}>'
 
 
 # "Eglise" class
@@ -135,12 +193,17 @@ class Eglise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(100), nullable=False)
-    paroisse_id = db.Column(db.Integer, db.ForeignKey('paroisse.id'), nullable=False)
-    paroisse = db.relationship('Paroisse', backref=db.backref('eglises', lazy=True))
-    
+    paroisse_id = db.Column(
+        db.Integer, db.ForeignKey('paroisse.id'), nullable=False
+    )
+    paroisse = db.relationship(
+        'Paroisse', backref=db.backref('eglises', lazy=True)
+    )
+
     def __repr__(self):
-        return f"<Eglise {self.name}>"
-    
+        return f'<Eglise {self.name}>'
+
+
 # "Type" class
 class Type(db.Model):
     """
@@ -153,32 +216,34 @@ class Type(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    
+
     def __repr__(self):
-        return f"<Type {self.name}>"
+        return f'<Type {self.name}>'
+
 
 # "Required" class
 class Required(db.Model):
     """
     Required class
-    
+
     Attributes:
     id : int : the id of the required relation
     type : Type : the type of celebration
     role : Role : the role required
     level : int : the level of the required role : 0 is absent, 1 is optional and 2 is required
     """
+
     id = db.Column(db.Integer, primary_key=True)
     type_id = db.Column(db.Integer, db.ForeignKey('type.id'), nullable=False)
     type = db.relationship('Type', backref=db.backref('required', lazy=True))
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
     role = db.relationship('Role', backref=db.backref('required', lazy=True))
-    level = db.Column(db.Integer, nullable=False) 
+    level = db.Column(db.Integer, nullable=False)
 
-    
     def __repr__(self):
-        return f"<Required {self.name}>"
-    
+        return f'<Required {self.name}>'
+
+
 # "Role" class
 class Role(db.Model):
     """
@@ -188,11 +253,13 @@ class Role(db.Model):
     id : int : the id of the role
     name : str : the name of the role
     """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    
+
     def __repr__(self):
-        return f"<Role {self.name}>"
+        return f'<Role {self.name}>'
+
 
 # "Celebration" class
 class Celebration(db.Model):
@@ -205,15 +272,24 @@ class Celebration(db.Model):
     eglise : Eglise : the eglise of the celebration
     type : Type : the type of the celebration
     """
+
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
-    eglise_id = db.Column(db.Integer, db.ForeignKey('eglise.id'), nullable=False)
-    eglise = db.relationship('Eglise', backref=db.backref('celebrations', lazy=True))
+    eglise_id = db.Column(
+        db.Integer, db.ForeignKey('eglise.id'), nullable=False
+    )
+    eglise = db.relationship(
+        'Eglise', backref=db.backref('celebrations', lazy=True)
+    )
     type_id = db.Column(db.Integer, db.ForeignKey('type.id'), nullable=False)
-    type = db.relationship('Type', backref=db.backref('celebrations', lazy=True))
+    type = db.relationship(
+        'Type', backref=db.backref('celebrations', lazy=True)
+    )
+
     def __repr__(self):
-        return f"<Celebration {self.date}>"
-    
+        return f'<Celebration {self.date}>'
+
+
 # "Intervenant_Celebration" class
 class Intervenant_Celebration(db.Model):
     """
@@ -224,15 +300,26 @@ class Intervenant_Celebration(db.Model):
     intervenant : Intervenant : the intervenant
     celebration : Celebration : the celebration
     """
-    id = db.Column(db.Integer, primary_key=True)
-    id_intervenant = db.Column(db.Integer, db.ForeignKey('intervenant.id'), nullable=False)
-    intervenant = db.relationship('Intervenant', backref=db.backref('intervenant_celebrations', lazy=True))
-    id_celebration = db.Column(db.Integer, db.ForeignKey('celebration.id'), nullable=False)
-    celebration = db.relationship('Celebration', backref=db.backref('intervenant_celebrations', lazy=True))
 
-    
+    id = db.Column(db.Integer, primary_key=True)
+    id_intervenant = db.Column(
+        db.Integer, db.ForeignKey('intervenant.id'), nullable=False
+    )
+    intervenant = db.relationship(
+        'Intervenant',
+        backref=db.backref('intervenant_celebrations', lazy=True),
+    )
+    id_celebration = db.Column(
+        db.Integer, db.ForeignKey('celebration.id'), nullable=False
+    )
+    celebration = db.relationship(
+        'Celebration',
+        backref=db.backref('intervenant_celebrations', lazy=True),
+    )
+
     def __repr__(self):
-        return f"<Intervenant_Celebration {self.intervenant.id} {self.celebration.id}>"
+        return f'<Intervenant_Celebration {self.intervenant.id} {self.celebration.id}>'
+
 
 class Intervenant_Role(db.Model):
     """
@@ -243,40 +330,74 @@ class Intervenant_Role(db.Model):
     intervenant : Intervenant : the intervenant
     role : Role : the role
     """
-    id = db.Column(db.Integer, primary_key=True)
-    intervenant_id = db.Column(db.Integer, db.ForeignKey('intervenant.id'), nullable=False)
-    intervenant = db.relationship('Intervenant', backref=db.backref('intervenant_roles', lazy=True))
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
-    role = db.relationship('Role', backref=db.backref('intervenant_roles', lazy=True))
 
-    
+    id = db.Column(db.Integer, primary_key=True)
+    intervenant_id = db.Column(
+        db.Integer, db.ForeignKey('intervenant.id'), nullable=False
+    )
+    intervenant = db.relationship(
+        'Intervenant', backref=db.backref('Intervenant_roles', lazy=True)
+    )
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
+    role = db.relationship(
+        'Role', backref=db.backref('Intervenant_roles', lazy=True)
+    )
+
     def __repr__(self):
-        return f"<Intervenant_Role {self.intervenant.id} {self.role.id}>"
+        return f'<Intervenant_Role {self.intervenant.id} {self.role.id}>'
+
 
 # ==== Decorators ==== #
+
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user_id' not in session:
+            session['next'] = request.url
             flash('You need to be logged in to access this page', 'danger')
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         return f(*args, **kwargs)
+
     return decorated_function
 
-def group_required(groups : list):
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            if 'user_id' not in session:
-                flash('You need to be logged in to access this page', 'danger')
-                return redirect(url_for('login'))
-            if Intervenant.query.filter(Intervenant.id == session['user_id']).first().is_role(groups):
-                flash('You do not have the right to access this page', 'danger')
-                return redirect(url_for('index'))
-            return f(*args, **kwargs)
-        return decorated_function
-    return decorator
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            session['next'] = request.url
+            flash('You need to be logged in to access this page', 'danger')
+            return redirect(url_for('index'))
+        if (
+            not Intervenant.query.filter(Intervenant.id == session['user_id'])
+            .first()
+            .is_role('admin')
+        ):
+            flash('You do not have the right to access this page', 'danger')
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# ==== Functions ==== #
+def page(route: str, user: Intervenant):
+    """
+    htmlpage function
+
+    This function is used to return the html code of a page.
+
+    Parameters:
+    path : str : the path of the page
+    user : Intervenant : the user who is requesting the page
+    """
+    if user.is_role('admin'):
+        template = AdminRoutes()
+    else:
+        template = Routes()
+    return getattr(template, route)
+
 
 # ==== Routes ==== #
 # Index route
@@ -287,12 +408,14 @@ def group_required(groups : list):
 def index():
     return render_template('index.html')
 
+
 # Login route
 # This route is used to login the user.
 # It is accessible by everyone.
 # It displays the login page.
 @app.route('/login', methods=['POST'])
 def login():
+    print('LOGIN EN COUR')
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -300,16 +423,27 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 session['user_id'] = user.id
-                flash('You are now logged in', 'success')
-                return redirect(url_for('index'))
+                app.logger.info(f'user {user.id} logged in')
+                return redirect(url_for(page('home', user)))
             else:
+                app.logger.warning(
+                    f'user {user.id} tried to log in with a wrong password'
+                )
                 flash('Invalid password', 'danger')
                 return redirect(url_for('index'))
         else:
+            app.logger.warning(f"user with email '{email}' not found")
             flash('User not found', 'danger')
             return redirect(url_for('index'))
-    else:
-        abort(405)
+
+
+# admin home route
+# This route is used to display the admin home page.
+# It is accessible by the admin.
+@app.route('/admin')
+@login_required
+def admin_home():
+    return render_template('home-admin.html')
 
 
 if __name__ == '__main__':
